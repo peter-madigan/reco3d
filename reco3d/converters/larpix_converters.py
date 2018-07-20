769814254629e081624882e14b8e59873e266676
Peter Madigan <pmadigan@berkeley.edu>
2018-07-20 11:35:51 -0700

    Clean code in larpix_converters

--- reco3d/converters/larpix_converters.py
+++ reco3d/converters/larpix_converters.py
@@ -52,7 +52,7 @@ class LArPixHDF5Converter(Converter):
         }
 
     def __init__(self, options):
-        super().__init__(options)
+        super(LArPixHDF5Converter, self).__init__(options)
         self.filename = self.options['filename']
         self.is_open = False
         self.datafile = None
@@ -94,15 +94,15 @@ class LArPixHDF5Converter(Converter):
             self.open()
         dset_name = self.type_lookup[dtype]
 
-        if dset_name is 'hits':
+        if dset_name == 'hits':
             hit = self.read_hit(loc)
             return hit
 
-        elif dset_name is 'tracks':
+        elif dset_name == 'tracks':
             track = self.read_track(loc)
             return track
 
-        elif dset_name is 'events':
+        elif dset_name == 'events':
             event = self.read_event(loc)
             return event
 
@@ -113,20 +113,21 @@ class LArPixHDF5Converter(Converter):
         Write data to location. If None specified, append to end of dataset
         return True if successful, False if not
         '''
-        if not type(data) in self.type_lookup: return False
+        if not isinstance(data, tuple(self.type_lookup.keys())):
+            return False
         if not self.is_open:
             self.open()
         dset_name = self.type_lookup[type(data)]
 
-        if dset_name is 'hits':
+        if dset_name == 'hits':
             self.write_hit(data, loc)
             return True
 
-        elif dset_name is 'tracks':
+        elif dset_name == 'tracks':
             self.write_track(data, loc)
             return True
 
-        elif dset_name is 'events':
+        elif dset_name == 'events':
             self.write_event(data, loc)
             return True
 
@@ -192,7 +193,8 @@ class LArPixHDF5Converter(Converter):
         Returns row index of object
         '''
         write_data = self.reco3d_type_to_hdf5(obj, **kwargs)
-        if write_data is None: raise TypeError('could not convert data type to array')
+        if write_data is None:
+            raise TypeError('could not convert data type to array')
         dset_name = self.type_lookup[type(obj)]
 
         if not dset_name in self.datafile.keys():
@@ -224,15 +226,17 @@ class LArPixHDF5Converter(Converter):
         track_idx = self.write_obj(track, track_idx, event_ref=event_ref, hit_ref=hit_ref)
         track_ref = self.datafile[track_dset_name].regionref[track_idx]
 
-        if not hit_ref is None: return track_idx
+        if not hit_ref is None:
+            return track_idx
         # store hits if no hit_ref is given
 
-        if hit_ref is None and len(track.hits) > 0:
+        if hit_ref is None and track.hits:
             # store hits
             hit_idcs = []
-            for hit in track.hits: hit_idcs += [self.write_hit(hit, None, event_ref=None, track_ref=track_ref)]
+            for hit in track.hits:
+                hit_idcs += [self.write_hit(hit, None, event_ref=None, track_ref=track_ref)]
             hit_dset_name = self.type_lookup[type(track.hits[0])]
-            if len(track.hits) > 0:
+            if track.hits:
                 hit_ref = self.datafile[hit_dset_name].regionref[hit_idcs]
             else:
                 hit_ref = None
@@ -246,21 +250,23 @@ class LArPixHDF5Converter(Converter):
         # write event in correct location with uninitialized references
         event_dset_name = self.type_lookup[type(event)]
         event_idx = loc
-        if event_idx is None: 
+        if event_idx is None:
             event_idx = self.write_idx[event_dset_name]
             self.write_idx[event_dset_name] += 1
         event_idx = self.write_obj(event, event_idx, track_ref=track_ref, hit_ref=hit_ref)
         event_ref = self.datafile[event_dset_name].regionref[event_idx]
 
-        if not track_ref is None and not hit_ref is None: return event_idx
+        if not track_ref is None and not hit_ref is None:
+            return event_idx
         # store hits and tracks if none are specified
 
         if hit_ref is None:
             # store hits
             hit_idcs = []
-            for hit in event.hits: hit_idcs += [self.write_hit(hit, None, event_ref=event_ref, track_ref=track_ref)]
+            for hit in event.hits:
+                hit_idcs += [self.write_hit(hit, None, event_ref=event_ref, track_ref=track_ref)]
             hit_dset_name = self.type_lookup[type(event.hits[0])]
-            if len(event.hits) > 0:
+            if event.hits:
                 hit_ref = self.datafile[hit_dset_name].regionref[hit_idcs]
             else:
                 hit_ref = None
@@ -270,13 +276,14 @@ class LArPixHDF5Converter(Converter):
         if track_ref is None:
             # store tracks
             track_idcs = []
-            tracks = [reco_obj for reco_obj in event.reco_objs if type(reco_obj) is reco3d_types.Track]
-            if len(tracks) > 0 and track_ref is None:
+            tracks = [reco_obj for reco_obj in event.reco_objs if isinstance(reco_obj, reco3d_types.Track)]
+            if tracks and track_ref is None:
                 track_dset_name = self.type_lookup[type(tracks[0])]
                 for track in tracks:
                     # find hits associated with track
                     hit_idcs = []
-                    for hit in track.hits: hit_idcs += [self.find(hit, hit_ref)]
+                    for hit in track.hits:
+                        hit_idcs += [self.find(hit, hit_ref)]
                     track_hit_ref = self.datafile[hit_dset_name].regionref[hit_idcs]
                     # write track with references
                     track_idx = self.write_obj(track, None, event_ref=event_ref, hit_ref=track_hit_ref)
@@ -285,7 +292,7 @@ class LArPixHDF5Converter(Converter):
                     hit_track_ref = self.datafile[track_dset_name].regionref[track_idx]
                     for hit_idx, hit in zip(hit_idcs, track.hits):
                         self.write_hit(hit, hit_idx, event_ref=event_ref, track_ref=hit_track_ref)
-            if len(tracks) > 0:
+            if tracks:
                 track_ref = self.datafile[track_dset_name].regionref[track_idcs]
             else:
                 track_ref = None
@@ -298,13 +305,14 @@ class LArPixHDF5Converter(Converter):
         Look for objects that match data object in a region.
         Return index of row that matches or None, if it can't be found
         '''
-        if not type(obj) in self.type_lookup.keys(): return None
+        if not isinstance(obj, tuple(self.type_lookup.keys())):
+            return None
         dset_name = self.type_lookup[type(obj)]
         region = search_region
         if region is None:
             # search all data
             region = self.datafile[dset_name].regionref[:]
-        elif isinstance(region, list) or isinstance(region, slice) or isinstance(region, int):
+        elif isinstance(region, (list, slice, int)):
             # search by indices
             region = self.datafile[dset_name].regionref[search_region]
         elif isinstance(region, h5py.RegionReference):
@@ -315,7 +323,8 @@ class LArPixHDF5Converter(Converter):
 
         for row_idx, row_data in enumerate(self.datafile[dset_name][region]):
             obj_data = self.hdf5_to_reco3d_type(type(obj), row_data)
-            if obj_data == obj: return row_idx
+            if obj_data == obj:
+                return row_idx
         return None
 
     def reco3d_type_to_hdf5(self, reco3d_type_obj, **kwargs):
@@ -323,7 +332,7 @@ class LArPixHDF5Converter(Converter):
         Automatically generate numpy array used by hdf5
         Additional attributes can be set (or overridden by kwargs)
         '''
-        if not type(reco3d_type_obj) in self.type_lookup.keys():
+        if not isinstance(reco3d_type_obj, tuple(self.type_lookup.keys())):
             return None
         data_dict = vars(reco3d_type_obj).copy()
         dataset_name = self.type_lookup[type(reco3d_type_obj)]
@@ -359,7 +368,7 @@ class LArPixHDF5Converter(Converter):
             args_dict[key] = data[key]
         items_to_add = []
         for key, value in args_dict.items():
-            if dset_name != 'hits' and type(value) is h5py.RegionReference and value:
+            if dset_name != 'hits' and isinstance(value, h5py.RegionReference) and value:
                 region = self.datafile[value][value]
                 if key == 'hit_ref':
                     items_to_add += [('hits', [self.hdf5_to_reco3d_type(\
@@ -367,7 +376,8 @@ class LArPixHDF5Converter(Converter):
                 elif key == 'track_ref':
                     items_to_add += [('reco_objs', [self.hdf5_to_reco3d_type(\
                             reco3d_types.Track, track_row) for track_row in region])]
-        for key, value in items_to_add: args_dict[key] = value
+        for key, value in items_to_add:
+            args_dict[key] = value
         self.replace_default_with_none(args_dict)
         obj = (dtype)(**args_dict)
         return obj
